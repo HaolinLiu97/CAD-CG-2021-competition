25201e1716b85df3011fd130d71fab0c135e938b
UncleMEDM <115010192@link.cuhk.edu.cn>
2021-06-23 11:26:59 +0800

    update evaluation script for depth estimation

--- evaluation/evaluate_depth/README.md
+++ evaluation/evaluate_depth/README.md
@@ -1,3 +1,10 @@
-#How to evaluate depth estimation
-The competitor will need to save the result which has the filename as `3dfront-dataset-batch-split-10k-0-cam-(index).png'. And put all the results depth image in one folder, zip it and submit them to us. <br>
+# How to evaluate depth estimation
+The competitor will need to save the result which has the filename as `3dfront-dataset-batch-split-10k-0-cam-(index)/3dfront-dataset-batch-split-10k-0-cam-(index).exr'(exr is recommended since it can store float image). And put all the results depth image in one folder, zip it and submit them to us. <br>
 Make sure the depth image is distance to camera plane in meter unit, and closer object have smaller depth value.
+
+## Run the evaluation program
+```angular2
+bash py_entrance.sh input_param.json output_report.json
+```
+The input_param includes the path of the answer directory and the predicted results directory. 
+Please make sure your predicted results is saved as the same format of the provided example in result_dir.
---
+++ evaluation/evaluate_depth/evaluate.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+import json
+import sys
+import os
+import cv2
+import numpy as np
+
+# 错误字典，这里只是示例
+error_msg={
+    1: "Bad input file",
+    2: "Wrong input file format",
+}
+
+def dump_2_json(info, path):
+    with open(path, 'w') as output_json_file:
+        json.dump(info, output_json_file,indent=1)
+
+def report_error_msg(detail, showMsg, out_p):
+    error_dict=dict()
+    error_dict['errorDetail']=detail
+    error_dict['errorMsg']=showMsg
+    error_dict['score']=0
+    error_dict['scoreJson']={}
+    error_dict['success']=False
+    dump_2_json(error_dict,out_p)
+
+def report_score(score, out_p):
+    result = dict()
+    result['success']=True
+    result['mmse'] = score
+
+    # 这里{}里面的score注意保留，但可以增加其他key，比如这样：
+    # result['scoreJson'] = {'score': score, 'aaaa': 0.1}
+
+    dump_2_json(result,out_p)
+
+def compute_mmse(result_image,gt_image):
+    error=(result_image-gt_image)**2
+    mmse=np.mean(error)
+    return mmse
+
+def evaluate_mmse(submit_path,standard_path):
+    image_list=os.listdir(standard_path)
+    total_mmse=0
+    total_count=0
+    for image_name in image_list:
+        gt_path=os.path.join(standard_path,image_name,"depth.png")
+        print(gt_path)
+        gt_image=cv2.imread(gt_path,cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
+        gt_image=(1-gt_image/255.0)*10
+        result_path=os.path.join(submit_path,image_name,"depth.exr")
+        result_image=cv2.imread(result_path,cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
+        mmse=compute_mmse(result_image,gt_image)
+        total_count+=1
+        total_mmse+=mmse
+    mean_mmse=total_mmse/total_count
+    return mean_mmse
+
+
+if __name__=="__main__":
+    '''
+      online evaluation
+    '''
+    in_param_path = sys.argv[1]
+    out_path = sys.argv[2]
+
+    # read submit and answer file from first parameter
+    with open(in_param_path, 'r') as load_f:
+        input_params = json.load(load_f)
+
+    # 标准答案路径
+    standard_path=input_params["fileData"]["standardFileDir"]
+
+    # 选手提交的结果文件路径
+    submit_path=input_params["fileData"]["userFileDir"]
+
+    mean_mmse=evaluate_mmse(submit_path,standard_path)
+    report_score(mean_mmse,out_path)
